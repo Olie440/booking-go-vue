d1d1dc874b9f053d4ec597d4487724e6e00cc4cb
Oliver Rushworth <oliver@oliverushworth.uk>
2019-08-03 14:34:21 +0100

    [#1] Add basic components and styling

---
+++ src/components/CarSearch.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="car-search screen background--blue">
+    <div class="car-search__search-container background--yellow">
+      <h2 class="car-search__header">Let’s find your ideal car</h2>
+      <label class="car-search__label" for="car-search__search">
+        Pick-up Location</label
+      >
+      <SearchResults />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import SearchResults from "./SearchResults.vue";
+
+@Component({
+  components: {
+    SearchResults
+  }
+})
+export default class CarSearch extends Vue {}
+</script>
+
+<style scoped>
+.car-search__search-container {
+  display: block;
+  width: 35rem;
+  margin: 7.5rem auto 0 auto;
+  padding: 1.75rem 1.5rem;
+}
+
+.car-search__header {
+  margin-bottom: 1.5rem;
+}
+
+.car-search__label {
+  display: inline-block;
+  margin-bottom: 0.5rem;
+  font-size: 14px;
+}
+
+@media (max-width: 756px) {
+  .car-search__search-container {
+    display: block;
+    width: calc(100% - 2rem);
+    margin: 1rem;
+  }
+}
+</style>
---
+++ src/components/SearchResults.vue
@@ -0,0 +1,38 @@
+<template>
+  <div className="search-results">
+    <input
+      aria-label="city, airport, station, region and district..."
+      class="search-results__input"
+      id="search-results"
+      placeholder="city, airport, station, region and district..."
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class SearchResults extends Vue {}
+</script>
+
+<style scoped>
+.search-results__input {
+  display: inline-block;
+  border: 1px solid #b4b4b4;
+  border-radius: 4px;
+  padding: 8px 16px;
+  font-size: 16px;
+  line-height: 1.5;
+  width: 100%;
+}
+
+.search-results__input:focus {
+  border-color: #0f6193;
+  outline: 0;
+}
+
+.search-results__input::placeholder {
+  font-size: 14px;
+}
+</style>
